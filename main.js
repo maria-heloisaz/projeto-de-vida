096aeef9940743bc4eb55cd680fb22120e3ba050
Maria Heloisa <112866821+maria-heloisaz@users.noreply.github.com>
2024-04-23 14:09:35 -0300

    ajustando conometro

--- main.js
+++ main.js
@@ -15,7 +15,23 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 let tempoAtual = new Date();
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
